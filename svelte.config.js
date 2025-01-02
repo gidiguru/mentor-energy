d9ab3cdcb35008bb47e7bb54c67a5123b8499771
gidiguru <81527773+gidiguru@users.noreply.github.com>
2025-01-02 08:21:56 -0600

    updated privacy policy, terms of service and cookie policy

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ status, path, referrer, message }) => {
+			  // Skip 404s instead of failing the build
+			  if (status === 404) {
+				console.warn(`Warning: Route ${path} not found (referred from ${referrer}). Skipping prerender.`);
+				return;
+			  }
+			  // Otherwise fail the build
+			  throw new Error(message);
+			}
+		  }
 	}
 };
 export default config;
