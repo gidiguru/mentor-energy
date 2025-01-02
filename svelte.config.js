8c1be740bc579cc40d3577b3cfe3241fea2f7e1d
gidiguru <81527773+gidiguru@users.noreply.github.com>
2025-01-02 12:28:03 -0600

    Removed google signin capability temporarily

--- svelte.config.js
+++ svelte.config.js
@@ -16,17 +16,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		prerender: {
-			handleHttpError: ({ status, path, referrer, message }) => {
-			  // Skip 404s instead of failing the build
-			  if (status === 404) {
-				console.warn(`Warning: Route ${path} not found (referred from ${referrer}). Skipping prerender.`);
-				return;
-			  }
-			  // Otherwise fail the build
-			  throw new Error(message);
-			}
-		  }
+
 	}
 };
 export default config;
